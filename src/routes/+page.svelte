aae950f3fb8c39fc6d94bfb0256c4b4b03e85355
Victor Digital <victordj587@gmail.com>
2024-11-18 13:51:33 +0100

    working fonts @Asguho

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
 	class="flex min-h-screen flex-col justify-between bg-[#1c2429] text-white *:w-full *:p-8 md:flex-row"
 >
 	<section class="border-r-4 border-gray-600">
-		<h1 class="text-9xl font-bold text-teal-400">Komma</h1>
+		<h1 class="font-feather text-9xl font-bold text-teal-400">Komma</h1>
 		<ul class="mt-4 *:my-12 *:text-3xl *:font-bold">
 			<li>Learn</li>
 			<li>Practice</li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,15 +4,14 @@ import typography from '@tailwindcss/typography';
 import defaultTheme from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
-
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ["Feather Bold", ...defaultTheme.fontFamily.sans],
-			},
+				feather: ['Feather Bold', ...defaultTheme.fontFamily.sans]
+			}
 		}
 	},
 
