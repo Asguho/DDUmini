2d64635060707317f49ed6e480b7be752f0f08ab
Victor Digital <victordj587@gmail.com>
2024-11-20 11:31:21 +0100

    Enhance user authentication flow and improve profile page styling

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -1,13 +1,22 @@
 import * as auth from '$lib/server/auth';
 import { fail, redirect } from '@sveltejs/kit';
+import { eq } from 'drizzle-orm';
+import { db } from '$lib/server/db';
+import * as table from '$lib/server/db/schema';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async (event) => {
 	if (!event.locals.user) {
 		return redirect(302, '/auth/login');
-	} 
-	
-	return { user: event.locals.user };
+	}
+
+	const [user] = await db.select().from(table.user).where(eq(table.user.id, event.locals.user.id));
+
+	if (!user) {
+		return redirect(302, '/auth/login');
+	}
+
+	return { user };
 };
 
 export const actions: Actions = {
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -3,10 +3,24 @@
 	import type { PageServerData } from './$types';
 
 	let { data }: { data: PageServerData } = $props();
+
+	console.log(data.user.activityHistory);
+	let activityData = JSON.parse(data.user.activityHistory || '{}');
+	console.log(activityData);
 </script>
 
-<h1>Hi, {data.user.username}!</h1>
-<p>Your user ID is {data.user.id}.</p>
-<form method="post" action="?/logout" use:enhance>
-	<button>Sign out</button>
-</form>
+<div class="flex min-h-screen min-w-full flex-col items-center justify-center p-4">
+	<h1 class="text-5xl font-bold text-teal-400">Profile</h1>
+	<div class="mt-4 w-full max-w-md rounded-3xl border-4 border-gray-600 p-8">
+		<h2 class="text-3xl font-bold">Hi, {data.user.username}!</h2>
+		<p class="text-lg text-gray-600">User ID: {data.user.id}</p>
+		<p class="text-lg text-gray-600">XP: {data.user.xp}</p>
+		<div></div>
+		<form method="post" action="?/logout" use:enhance class="mt-4">
+			<button class="w-full rounded-lg bg-teal-400 p-2 text-2xl font-bold text-black"
+				>Sign out</button
+			>
+		</form>
+		<a href="/" class="mt-4 block text-center text-teal-400 underline">Go Back</a>
+	</div>
+</div>
