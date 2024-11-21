e00f4f4d2f88f822f8821dda88223c4c0261134f
Victor Digital <victordj587@gmail.com>
2024-11-21 14:01:36 +0100

    Sort users by descending experience points in the top page server load

--- src/routes/top/+page.server.ts
+++ src/routes/top/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 import { db } from '$lib/server/db';
 import * as table from '$lib/server/db/schema';
-import { eq, sql } from 'drizzle-orm';
+import { eq, sql, desc } from 'drizzle-orm';
 import { redirect } from '@sveltejs/kit';
 
 export const load = (async (event) => {
@@ -9,6 +9,6 @@ export const load = (async (event) => {
 		throw redirect(302, '/auth');
 	}
 	//get all users and rank based of xp
-	const users = await db.select().from(table.user).orderBy(table.user.xp);
+	const users = await db.select().from(table.user).orderBy(desc(table.user.xp));
 	return { users };
 }) satisfies PageServerLoad;
